ec012fb8072b571d6a3580bf327a160281deb764
HanEmile <emile.hansmaennel@gmail.com>
2017-07-10 14:14:45 +0200

    had to commit to get rid of error-message

--- clean/heidelberg.py
+++ clean/heidelberg.py
@@ -5,16 +5,44 @@ import numpy as np
 from variables import *
 from constants import *
 
+# rho function
+def rho(r):
+    a = (1) / (math.sqrt( 2 * pi ) * sigma )
+    b = math.exp( - (phi(r) / sigma ** 2 ) )
+    c = a * b
+    return c
+
 # phi function
 def phi(x):
+    # if x < 0:
+    #     x = -x # -x = --x = +x
     a = - ( 4 * pi * G * f_0 * R_s ** 3 ) / x
     b = np.log(1. + (x / R_s) )
     c = a * b
     return c
 
-# rho function
-def rho(r):
-    a = (1) # / (math.sqrt( 2 * pi ) * sigma )
-    b = math.exp( - (phi(r) / sigma ** 2 ) )
+def phi_3d(x,y,z):
+    r = sqrt(x**2 + y**2 + z**2)
+    a = - ( 4 * pi * G * f_0 * R_s ** 3 ) / r
+    b = np.log(1. + (r / R_s) )
     c = a * b
     return c
+
+def DxDxphi(x, y, z):
+    h = 0.01 # pc
+    result = phi(x+h, y, z) - 2 * phi(x, y, z) + phi(x-h, y, z)
+    result = result / h**2
+    return result
+
+def DyDyphi(x, y, z):
+    h = 0.01 # pc
+    result = phi(x, y+h, z) - 2 * phi(x, y, z) + phi(x, y-h, z)
+    result = result / h**2
+    return result
+
+def DxDyphi(x, y, z):
+    h = 0.01 # pc
+    k = 0.01 # pc
+    result = phi(x+h, y+k, z) - phi(x+h, y-k, z) - phi(x-h, y+k, z) + phi(x-h, y-k, z)
+    result = result / (4*h*k)
+    return result
--- clean/variables.py
+++ clean/variables.py
@@ -1,4 +1,4 @@
 # variables
 sigma = 200
-f_0 = 1
+f_0 = 0.1
 R_s = 1e4
