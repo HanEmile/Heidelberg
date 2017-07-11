61b1a379527b5adbc7e9c5360c0513878b4fd44e
HanEmile <emile.hansmaennel@gmail.com>
2017-07-11 17:21:09 +0200

    custom random distribution not working D;

--- clean/main_proto.py
+++ clean/main_proto.py
@@ -24,6 +24,7 @@ stars = int(1e3)
 
 # lists / arrays
 lista = []
+listb = []
 arr_rand_pos = np.zeros((stars, 3))
 
 # create new file for every calculation
@@ -38,7 +39,7 @@ def gen_star(star_nr):
     temp_arr = np.zeros((3))
 
     for axis in range(0, 3):
-        rand_val = np.random.uniform(int(1e1), int(1e12), size=1)
+        rand_val = np.random.uniform(int(0), int(1e15), size=1)
         arr_rand_pos[star_nr][axis] = rand_val
 
     x = arr_rand_pos[star_nr][0]
@@ -48,12 +49,19 @@ def gen_star(star_nr):
     temp_arr[0] = x
     temp_arr[1] = y
     temp_arr[2] = z
-    # print(temp_arr)
 
-    rand_val_check = np.random.uniform(0, 20, size=1)
-    print("{:<20}{:<20}".format(str(rand_val_check), str(hd.rho3d(x, y, z)) ))
+    # r = math.sqrt(x**2 + y**2 + z**2)
+    # lista.append(hd.rho(r))
 
-    if rand_val_check > hd.rho3d(x, y, z):
+    rand_val_check = np.random.uniform(0, 1500, size=1)
+    r = math.sqrt(x**2 + y**2 + z**2) / int(1e7)
+    rho_r = hd.rho(r)
+
+    print("{:<20}{:<20}{:<20}".format(x, y, z), end="")
+    print("{:<20}{:<20}".format( str(hd.rho3d(x, y, z)), str(rand_val_check) ), end="")
+    print("{:<20}{:<20}".format(r, rho_r))
+
+    if rand_val_check < rho_r:
         with open(path, "a") as data:
             data.write( str(temp_arr[0]).strip("[]") + "," )
             data.write( str(temp_arr[1]).strip("[]") + "," )
@@ -64,15 +72,19 @@ def gen_star(star_nr):
 for star in range(0, stars):
     gen_star(star)
 
+print("{:<20}{:<20}{:<20}".format("x", "y", "z"), end="")
+print("{:<20}{:<20}{:<20}{:<20}".format("hd.rho3d(x, y, z)", "rand_val_check", "r", "hd.rho(r)"))
 print("")
-
-# arr_log = np.logspace(1, 7, int(1e5))
-# print(len(arr_log))
 #
-# for i in arr_log:
-#     print("{:<10}{:<20}".format(i, hd.rho3d(i, i, i)))
-#     lista.append(hd.rho3d(i, i, i))
+# arr_r = np.logspace(0, 10, int(1e5))
+#
+# for r in arr_r:
+#     print(r)
+#     listb.append(hd.rho(r))
 #
 # plt.plot(lista)
+# plt.plot(listb)
+#
 # plt.xscale('log')
+# plt.legend(["lista", "listb"])
 # plt.show()
