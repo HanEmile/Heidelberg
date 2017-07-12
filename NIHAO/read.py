d95cc1ac2a949a30e4d1cf8d06663d07c3a9236d
HanEmile <emile.hansmaennel@gmail.com>
2017-07-12 16:42:05 +0200

    works as intended

--- NIHAO/read.py
+++ NIHAO/read.py
@@ -1,49 +1,67 @@
 from numpy import genfromtxt
 import bpy
 import os
+# from multiprocessing import Pool
 
+
+# First three lines of a data-set
+#
 # NIHAO simulation (Wang et al. 2015) data: stars of g1.12e12.01024
 # x[kpc] y[kpc] z[kpc] vx[kms^-1] vy[kms^-1] vz[kms^-1]
 # 0.011 -0.158 -0.389 95.335 -370.055 -287.738
+#
+# Blender 1 length-unit -> 1 kpc
 
 # controll
 star_size = 0.01
-step = int(1e4)
+step = int(1e5) # int(5e3) # int(5e2)
+time_step = 1.5e14 # s
+parsec = 3.086e16 # m
 
 g1 = 'data/g1.12e12_stars.txt'
 dm1 = 'data/g1.12e12_dm.txt'
 g2 = 'data/g2.79e12_stars.txt'
 dm2 = 'data/g2.79e12_dm.txt'
 
-# data = genfromtxt(g1, delimiter=' ')
-data = genfromtxt(g1, delimiter=' ')
-data_dm = genfromtxt(dm1, delimiter=' ')
-
+# create star at the given coordiantes
 def create_star(x, y, z):
     bpy.ops.mesh.primitive_cube_add( radius=star_size, location=(x, y, z) )
-    bpy.context.object.name = "s"
+    bpy.context.object.name = "s" + str(i)
 
+# create "dark matter" at the given coordinates
 def create_dark_matter(x, y, z):
     bpy.ops.mesh.primitive_cube_add( radius=star_size, location=(x, y, z) )
-    bpy.context.object.name = "dm"
+    bpy.context.object.name = "dm" + str(i)
 
-# switch to layer 0 to generate the stars
+# get data from files
+data = genfromtxt(g2, delimiter=' ')
+data_dm = genfromtxt(dm2, delimiter=' ')
+
+# define sce
+sce = bpy.context.scene
+sce.frame_set(0)
+
+# create stars
 for i in range(0, len(data), step):
+    # get the position data of the star
     x = data[i][0]
     y = data[i][1]
     z = data[i][2]
 
-    print("{:<7}{:<7}{:<7}".format(x, y, z), end="")
+    # print some information
+    print("{:<10}{:<10}{:<10}".format(x, y, z), end="")
     print("{:<7}{:<7}{:<3}{:<7}".format(" ", i, " / ", len(data) ))
+
+    # create the star
     create_star(x, y, z)
 
-# switch to layer 1 to generate the dark matter
+# create dark matter
 for i in range(0, len(data_dm), step):
     x = data_dm[i][0]
     y = data_dm[i][1]
     z = data_dm[i][2]
 
-    print("{:<7}{:<7}{:<7}".format(x, y, z), end="")
+    print("{:<10}{:<10}{:<10}".format(x, y, z), end="")
     print("{:<7}{:<7}{:<3}{:<7}".format(" ", i, " / ", len(data_dm) ))
     create_dark_matter(x, y, z)
 
