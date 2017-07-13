3880a4ece0cbe1b912480f00395484b7f6461b9c
Tim M. Tugendhat <timtugendhat@gmail.com>
2017-07-13 16:33:29 +0200

    finally fixed submit_stars?

--- clean/submit_stars.sh
+++ clean/submit_stars.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
+#hosts: structure03 cosmo35 planet04 cosmo33 cosmo02
+
+user=$(whoami)
 thisfolder=$(pwd)
-#for host in {structure03 cosmo35 planet04 cosmo33 cosmo02}; do
-#for host in {structure03 cosmo35 planet04 cosmo02}; do
+
 for host in $@; do
     cd $thisfolder
-    ssh hanemile@${host}.ita.uni-heidelberg.de > /dev/null<<EOF
+    ssh ${user}@${host}.ita.uni-heidelberg.de > /dev/null 2> /dev/null <<EOF
 cd $thisfolder
-#for i in 1 2 3 4; do
-nohup python3 gen.py &
-#done
+for i in 1 2 3 4; do
+nohup python3 gen.py > log 2> log.err < /dev/null &
+done
 EOF
     echo "submitted on ${host}!"
 done
