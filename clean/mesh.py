6c4e8431aa597968d1e05497308f332e11533270
HanEmile <emile.hansmaennel@gmail.com>
2017-07-15 19:56:49 +0200

    updated mesh.py

---
+++ clean/mesh.py
@@ -0,0 +1,44 @@
+import bpy
+from numpy import genfromtxt
+import os
+import sys
+
+directory = "data/" + sys.argv[5] + "/"
+print(directory)
+
+files = []
+
+print("### \n\n")
+
+for data_file in os.listdir(directory):
+    files.append(data_file)
+    print(data_file)
+
+print("### \n\n")
+
+for data in files:
+    path = str(directory) + str(data)
+    print(data)
+
+    verts = genfromtxt(path, delimiter=',', skip_header=0)
+
+    # create mesh and object
+    mesh = bpy.data.meshes.new(data)
+    object = bpy.data.objects.new(data,mesh)
+
+    # set mesh location
+    object.location = bpy.context.scene.cursor_location
+    bpy.context.scene.objects.link(object)
+
+    # create mesh from python data
+    mesh.from_pydata(verts,[],[])
+    mesh.update(calc_edges=True)
+
+bpy.ops.object.select_all(action='SELECT')
+
+for area in bpy.context.screen.areas:
+    if area.type == 'VIEW_3D':
+        for region in area.regions:
+            if region.type == 'WINDOW':
+                override = {'area': area, 'region': region, 'edit_object': bpy.context.edit_object}
+                bpy.ops.view3d.view_all(override)
